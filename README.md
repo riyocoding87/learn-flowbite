d47b3ee97bce7011096b111dd90bd246e3d82eb4
riyo <riyo@5000minds.com>
2022-03-24 00:46:16 +0700

    learn how to use tailwind

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # learn-flowbite
 learning flowbite - tailwindcss component
+
+currently i'm learning tailwindcss to make a template for my site. usually i'm familiar with bootstrap. and then i want to try another ui framework such as tailwind.
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./src/*/*.html",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
